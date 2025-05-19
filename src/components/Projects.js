8535a95ee90b801a63e68af86755ecd1c94075ce
VenkataSarma <venkatasarmavedam@gmail.com>
2025-05-19 09:07:33 +0530

    changed some code in css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,20 +4,33 @@ import { Link } from 'react-router-dom';
 function Projects() {
   return (
     <div id="Projects">
-    <div>
-      <div className="me">
-        <h1 className="about">Projects</h1>
+      <div>
+        <div className="me">
+          <h1 className="about">Projects</h1>
+        </div>
+        <div className="work">
+          <Link
+            to="/webdevelopment"
+            className="project web-development"
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL + '/Assets/WEB_DEVELOPMENT.png'})`,
+            }}
+          >
+            {/* You can add text here if you want */}
+          </Link>
+
+          <Link
+            to="/datascience"
+            className="project data-science"
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL + '/Assets/DATA_SCIENCE.png'})`,
+            }}
+          >
+            {/* You can add text here if you want */}
+          </Link>
+        </div>
       </div>
-      <div className="work">
-        <Link to="/webdevelopment" className="project web-development">
-          
-        </Link>
-        <Link to="/datascience" className="project data-science">
-           
-        </Link>
-         
-      </div>
-    </div></div>
+    </div>
   );
 }
 
